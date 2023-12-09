35687da9d4abb32fba34b94188268c09f20a14d6
Juan Guerrero <JCystems@gMail.com>
2023-12-08 22:06:03 -0600

    WiP

---
+++ tests/step_defs/pytest_bdd_parser/test_parse.py
@@ -0,0 +1,20 @@
+from pytest_bdd import parsers
+
+# FIXME
+def _test__parse__two_sum__given():
+    oParse = parsers.parse("an {array} of integer numbers")
+    result = oParse.parse_arguments('[2, 7, 11, 15]')
+    expected = "an [2, 7, 11, 15] of integer numbers"
+    assert result == expected, f"expected:{expected}, got:{result}"
+
+def test_format():
+    array_string = '[2, 7, 11, 15]'
+    actual_string = "an {array} of integer numbers".format(array=array_string)
+    expected_string = "an [2, 7, 11, 15] of integer numbers"
+    assert actual_string == expected_string, \
+        f"expected:{expected_string}, got:{actual_string}"
+
+    actual_array = eval(array_string)
+    expected_array = [2, 7, 11, 15]
+    assert actual_array == expected_array, \
+        f"expected:{expected_array}, got:{actual_array}"
--- tests/step_defs/test__leetcode__two_sum.py
+++ tests/step_defs/test__leetcode__two_sum.py
@@ -16,30 +16,37 @@ validate = {
     "target": lambda target: assert_is_in_range(target, constraints["target"]),
 }
 
-
-# @given(
-#     parsers.parse("an {array} of integer numbers"),
-#     converters={"array": eval},
-#     target_fixture="test_case",
-# )
-# def given_array(array):
-#     length = len(array)
-#     validate["length"](length)
-#     map(validate["value"], array)
-#     return {"nums": array}
+def parse_numbers(lines):
+    first_line = lines.split("\n")[0]
+    return [
+        int(number)
+        for number in
+        first_line.split(", ")
+    ]
 
 
 @given(
-    parsers.parse("an [2, 7, 11, 15] of integer numbers"),
+    parsers.parse("an {array} of integer numbers"),
+    converters={"array": eval},
     target_fixture="test_case",
 )
-def given_array():
-    array = [2, 7, 11, 15]
+def given_array(array):
     length = len(array)
     validate["length"](length)
     map(validate["value"], array)
     return {"nums": array}
 
+
+@given(
+    parsers.parse("some integer {numbers}"),
+    converters={"numbers": parse_numbers},
+    target_fixture="test_case",
+)
+def given_array(array):
+    length = len(array)
+    validate["length"](length)
+    map(validate["value"], array)
+    return {"nums": array}
 
 
 @given(parsers.parse("an integer {target:d} number"), target_fixture="test_case")
@@ -49,7 +56,7 @@ def given_target(test_case, target):
     return test_case
 
 
-@when("I call two_sum", target_fixture="test_case")
+@when("two_sum is invoked", target_fixture="test_case")
 def when__two_sum(test_case):
     nums = test_case["nums"]
     target = test_case["target"]
@@ -58,11 +65,32 @@ def when__two_sum(test_case):
 
 
 @then(
-    parsers.parse("return {indices} of the {two_numbers}"),
+    parsers.parse("it returns a tuple of {indices} of the {two_numbers}"),
     target_fixture="test_case",
     converters=dict(
         indices=eval,
-        two_numbers=eval,
+        two_numbers=parse_numbers,
+    ),
+)
+def then_returns_indeces(test_case, indices, two_numbers):
+    idx1, idx2 = indices
+    expected_num1, expected_num2 = two_numbers
+    nums = test_case["nums"]
+    num1 = nums[idx1]
+    num2 = nums[idx2]
+    test_case["num1"] = num1
+    test_case["num2"] = num2
+    assert expected_num1 == num1, f"expected:{expected_num1}, got:{num1}"
+    assert expected_num2 == num2, f"expected:{expected_num2}, got:{num2}"
+    return test_case
+
+
+@then(
+    parsers.parse("it returns a 2 {indices} of the {two_numbers}"),
+    target_fixture="test_case",
+    converters=dict(
+        indices=parse_numbers,
+        two_numbers=parse_numbers,
     ),
 )
 def then_returns_indeces(test_case, indices, two_numbers):
