c9127de9b6d9de7662dc371418a5ad090e38abc2
Juan Guerrero <JCystems@gMail.com>
2024-01-14 19:54:33 -0600

    Update test_parse.py

--- tests/pytest_bdd_parser/test_parse.py
+++ tests/pytest_bdd_parser/test_parse.py
@@ -1,3 +1,4 @@
+from hamcrest import assert_that, equal_to
 from pytest_bdd import parsers
 
 
@@ -6,19 +7,15 @@ def _test__parse__two_sum__given():
     oParse = parsers.parse("an {array} of integer numbers")
     result = oParse.parse_arguments("[2, 7, 11, 15]")
     expected = "an [2, 7, 11, 15] of integer numbers"
-    assert result == expected, f"expected:{expected}, got:{result}"
+    assert_that(result, equal_to(expected))
 
 
 def test_format():
     array_string = "[2, 7, 11, 15]"
     actual_string = "an {array} of integer numbers".format(array=array_string)
     expected_string = "an [2, 7, 11, 15] of integer numbers"
-    assert (
-        actual_string == expected_string
-    ), f"expected:{expected_string}, got:{actual_string}"
+    assert_that(actual_string, equal_to(expected_string))
 
     actual_array = eval(array_string)
     expected_array = [2, 7, 11, 15]
-    assert (
-        actual_array == expected_array
-    ), f"expected:{expected_array}, got:{actual_array}"
+    assert_that(actual_array, equal_to(expected_array))
