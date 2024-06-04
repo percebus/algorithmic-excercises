c2dbd451434cae3cec76f79d9f1ad27614897dc7
JC Guerrero <JCystems@gMail.com>
2024-06-04 18:53:10 -0500

    feat(Palindrome Number)

--- src/problems/leetcode/easy/two_sum.py
+++ src/problems/leetcode/easy/two_sum.py
@@ -2,18 +2,16 @@ from typing import Optional
 
 from hamcrest import assert_that, equal_to
 
-"""
-SRC: https://leetcode.com/problems/two-sum/
+# SRC: https://leetcode.com/problems/two-sum/
+#
+# Given an array of integers nums
+# and an integer target,
+# return indices of the two numbers such that they add up to target.
 
-Given an array of integers nums
-and an integer target,
-return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution,
+# and you may not use the same element twice.
 
-You may assume that each input would have exactly one solution,
-and you may not use the same element twice.
-
-You can return the answer in any order.
-"""
+# You can return the answer in any order.
 
 
 def two_sum(nums: list[int], target: int) -> Optional[list[int]]:
---
+++ tests/step_defs/problems/leetcode/easy/test__palindrome_number.py
@@ -0,0 +1,56 @@
+from pytest_bdd import given, parsers, scenarios, then, when
+
+from src.problems.leetcode.easy.palindrome_number import is_palindrome
+from tests.utils import assert_is_in_range
+
+constraints = {
+    "value": {  # -2^31 <= x <= 2^31 -1
+        "min": -(2**31),
+        "max": (2**31) - 1,
+    }
+}
+
+validate = {"value": lambda x: assert_is_in_range(x, constraints["value"])}
+
+
+scenarios("problems/leetcode/easy/Palindrome Number.feature")
+
+
+@given(parsers.parse("an integer number {number}"), converters={"number": eval}, target_fixture="context")
+def given_a_valid_number(number):
+    validate["value"](number)
+    return {"number": number}
+
+
+@given(parsers.parse("an invalid integer number {number}"), converters={"number": eval}, target_fixture="context")
+def given_an_invalid_number(number):
+    try:
+        validate["value"](number)
+        return {"number": number}
+    except Exception as exception:
+        return {"exception": exception}
+
+
+@when("is_palindrome is invoked")
+def when__is_palindrome__is_invoked(context):
+    number = context["number"]
+    context["result"] = is_palindrome(number)
+    return context
+
+
+@then("return True if x is palindrome integer")
+def then_returns_True_if_number_is_palindrome(context):
+    result = context["result"]
+    assert result is True
+
+
+@then("return False if x is NOT palindrome integer")
+def then_returns_False_if_number_is_not_palindrome(context):
+    result = context["result"]
+    assert result is False
+
+
+@then("handle the exception for the very large number")
+def then_it_handles_the_exception_for_very_large_number(context):
+    exception = context["exception"]
+    assert isinstance(exception, Exception), f"expected:Exception, got:{exception}"
