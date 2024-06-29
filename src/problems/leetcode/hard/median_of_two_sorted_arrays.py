f9d3bb8bf08e7d04dbd44593c4890616211b527d
JC Guerrero <JCystems@gMail.com>
2024-06-29 09:44:10 -0500

    feat(Median of Two Sorted Arrays) (#8)

---
+++ src/problems/leetcode/hard/median_of_two_sorted_arrays.py
@@ -0,0 +1,55 @@
+import math
+
+from hamcrest import assert_that, equal_to
+
+"""
+SRC: https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+Given two sorted arrays nums1 and nums2
+of size m and n respectively,
+return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)). # TODO TEST
+"""
+
+
+def find_median(nums1: list[int], nums2: list[int]) -> float:
+    nums3 = nums1 + nums2
+    nums3.sort()
+    size = len(nums3)
+    mid = math.ceil(size / 2)
+    is_pair = (size % 2) == 0
+    idx = mid - 1
+
+    if not is_pair:
+        return nums3[idx]
+
+    # implicit else
+    return (nums3[idx] + nums3[idx + 1]) / 2
+
+
+def test(nums1: list[int], nums2: list[int], expected: float):
+    result = find_median(nums1, nums2)
+    assert_that(result, equal_to(expected))
+    print("✅", end="")
+
+
+def run():
+    # Example 1:
+    # - Input: nums1 = [1,3], nums2 = [2]
+    # - Output: 2.00000
+    # - Explanation: merged array = [1,2,3] and median is 2.
+    test([1, 3], [2], expected=2)
+
+    # Example 2:
+    # - Input: nums1 = [1,2], nums2 = [3,4]
+    # - Output: 2.50000
+    # - Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+    test([1, 2], [3, 4], expected=2.5)
+
+    test([1, 2, 3, 4, 5], [], expected=3)
+
+    print("\n")
+
+
+if __name__ == "__main__":
+    run()
---
+++ tests/step_defs/problems/leetcode/hard/test__median_of_two_sorted_arrays.py
@@ -0,0 +1,79 @@
+from hamcrest import assert_that, equal_to, instance_of, is_
+from pytest_bdd import given, parsers, scenarios, then, when
+
+from src.problems.leetcode.hard import median_of_two_sorted_arrays
+from tests.utils import assert_is_in_range
+
+scenarios("problems/leetcode/hard/Median of Two Sorted Arrays.feature")
+
+constraints = {
+    "m": {"min": 0, "max": 1000},  # 0 <=   m   <= 1000
+    "m+n": {"min": 1, "max": 2000},  # 1 <= m + n <= 2000
+}
+
+
+def validate_list(array, expected):
+    length = len(array)
+    assert length == expected, f"len({array}): expected:{expected}, got:{length}"
+    validate["m"](length)
+
+
+def validate_lists(array1, array2):
+    m = len(array1)
+    n = len(array2)
+    compound = m + n
+    validate["m+n"](compound)
+
+
+validate = {
+    "m": lambda m: assert_is_in_range(m, constraints["m"]),
+    "m+n": lambda x: assert_is_in_range(x, constraints["m+n"]),
+    "list": validate_list,
+    "lists": validate_lists,
+}
+
+
+@given(parsers.parse("two sorted arrays {nums1} and {nums2}"), converters={"nums1": eval, "nums2": eval}, target_fixture="context")
+def given_two_sorted_arrays(nums1, nums2):
+    validate["lists"](nums1, nums2)
+    return {"nums1": nums1, "nums2": nums2}
+
+
+@given(parsers.parse("two empty arrays {nums1} and {nums2}"), converters={"nums1": eval, "nums2": eval}, target_fixture="context")
+def given_two_empty_arrays(nums1, nums2):
+    context = {"nums1": nums1, "nums2": nums2}
+    try:
+        validate["lists"](nums1, nums2)
+    except Exception as oException:
+        context["exception"] = oException
+
+    return context
+
+
+@given(parsers.parse("of size {m} and {n} respectively"), converters={"m": int, "n": int})
+def given_of_size_m_an_n(m, n, context):
+    nums1 = context["nums1"]
+    nums2 = context["nums2"]
+    validate["list"](nums1, m)
+    validate["list"](nums2, n)
+
+
+@when("I call find_median")
+def when_I_call__find_median(context):
+    nums1 = context["nums1"]
+    nums2 = context["nums2"]
+    result = median_of_two_sorted_arrays.find_median(nums1, nums2)
+    context["result"] = result
+    return context
+
+
+@then(parsers.parse("return the {median} of the two sorted arrays {merged}"), converters={"median": float, "merged": eval})
+def then_it_returns_the_median(median, merged, context):
+    result = context["result"]
+    assert_that(median, equal_to(result))
+
+
+@then("handle the exception for two empty arrays")
+def then_it_handles_the_exception(context):
+    oException = context["exception"]
+    assert_that(oException, is_(instance_of(Exception)))
