35978d376c8f8e32ff0b6100afe36046158a580f
JC Guerrero <JCystems@gMail.com>
2024-07-16 20:00:36 -0500

    style(prettier): .releaserc.mjs

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -23,8 +23,8 @@ export default {
       "@semantic-release/github",
       {
         assets: [
-          {path: "CHANGELOG"}, // XXX?
-          {path: "dist/*.tgz"}
+          { path: "CHANGELOG" }, // XXX?
+          { path: "dist/*.tgz" },
         ],
       },
     ],
