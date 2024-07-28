043d49a5086f134720628268eb31dbc0298104d1
JC Guerrero <JCystems@gMail.com>
2024-07-28 14:34:37 -0500

    refactor(leetcode/.../palindrome_number)

--- src/problems/leetcode/easy/palindrome_number/__main__.py
+++ src/problems/leetcode/easy/palindrome_number/__main__.py
@@ -1,16 +1,12 @@
 from hamcrest import assert_that, equal_to
 
+from src.problems.leetcode.easy.palindrome_number.v1 import is_palindrome
+
 # SRC: https://leetcode.com/problems/palindrome-number/
 #
 # Given an integer x, return true if x is palindrome integer.
 # An integer is a palindrome when it reads the same backward as forward.
 # For example, 121 is a palindrome while 123 is not.
-
-
-def is_palindrome(number: int) -> bool:
-    str1 = str(number)
-    str2 = str1[::-1]
-    return str1 == str2
 
 
 def test(number: int, expected: bool):
