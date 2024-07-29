cfe4c428c67bb1fd72e092c71897e949bce5143d
JC Guerrero <JCystems@gMail.com>
2024-07-28 21:38:11 -0500

    refactor!: meta/.../sum_abc
    BREAKING CHANGE

---
+++ src/problems/meta/coding/puzzles/warmup/sum_abc/v1.py
@@ -0,0 +1,16 @@
+"""
+Given three integers A, B, and C, determine their sum.
+
+Your task is to implement the function getSum(A, B, C)
+which returns the sum A+B+C.
+"""
+
+
+def calculate_sum(a: int, b: int, c: int) -> int:
+    return a + b + c
+
+
+# pylint: disable=invalid-name
+def getSum(A: int, B: int, C: int) -> int:
+    return calculate_sum(A, B, C)
+# pylint: enable=invalid-name
