1250d4d250490310287fd08780bdd2e78473c636
JC Guerrero <JCystems@gMail.com>
2024-08-17 11:25:40 -0500

    docs(multiples_of_3_and_5): Added text

--- src/problems/euler/multiples_of_3_or_5/__main__.py
+++ src/problems/euler/multiples_of_3_or_5/__main__.py
@@ -14,8 +14,12 @@ def test(limit: int, expected: Optional[int] = None) -> None:
 
 
 def run() -> None:
-    test(10, 23)
-    test(1000, 233168)
+    # If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    # we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    test(10, expected=23)
+
+    # Find the sum of all the multiples of 3 or 5 below 1000.
+    test(1000, expected=233168)
 
 
 if __name__ == "__main__":
