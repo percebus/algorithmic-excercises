53d28285dfc6daa9a0a3bab049a6e7553159c7b2
JC Guerrero <JCystems@gMail.com>
2025-01-03 16:03:37 -0600

    feat(leetcode/easy/reverse_strings)

---
+++ src/problems/leetcode/easy/reverse_string/v1.py
@@ -0,0 +1,24 @@
+from src.problems.leetcode.easy.reverse_string.protocol import SolutionProtocol
+
+
+class Solution:  # (SolutionProtocol)
+    def reverseString(self, s: list[str]) -> None:
+        """
+        SolutionProtocol.reverseString
+
+        Given an array of characters s, it modifies the same array.
+
+        Parameters:
+          - `s` (`list[str]`): An array of string characters.
+
+        Returns: `None`
+        """
+
+        chars = s
+        # FIXME simplify do 1 trip, instead of 2
+        inverted = chars[::-1]
+        for idx, item in enumerate(inverted):
+            s[idx] = item  # type: ignore
+
+
+solution: SolutionProtocol = Solution()
---
+++ tests/step_defs/problems/leetcode/easy/test__reverse_string.py
@@ -0,0 +1,32 @@
+from hamcrest import assert_that, equal_to, is_
+from pytest_bdd import given, parsers, scenarios, then, when
+
+from src.problems.leetcode.easy.reverse_string import reverse_string
+
+scenarios("problems/leetcode/easy/Reverse String.feature")
+
+
+@given(parsers.parse("an array of {chars}"), target_fixture="context")
+def given_an_array_of_chars(chars):
+    return {"chars": chars}
+
+
+@when("I call reverseString")
+def when_I_call_reverseString(context):
+    chars = context["chars"]
+    copy = list(chars)
+    context["result"] = reverse_string(copy)
+    context["reeversed"] = copy
+
+
+@then("the void function returns None")
+def then_it_returns_None(context):
+    result = context["result"]
+    assert_that(result, is_(None))
+
+@then(parsers.parse("the same array is procedurally {expected_reversed} by reference"))
+def then_the_string_got_reversed(expected_reversed, context):
+    reversed = context["reversed"]
+    reversed_chars = reversed.split(',')
+
+    assert_that(expected_reversed, equal_to(reversed_chars))
