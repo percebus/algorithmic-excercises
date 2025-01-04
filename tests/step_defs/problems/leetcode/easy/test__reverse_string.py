b9f9be1dffde9429aa44770904faf46a5ad6c4f8
JC Guerrero <JCystems@gMail.com>
2025-01-04 10:01:30 -0600

    Update test__reverse_string.py

--- tests/step_defs/problems/leetcode/easy/test__reverse_string.py
+++ tests/step_defs/problems/leetcode/easy/test__reverse_string.py
@@ -6,7 +6,7 @@ from src.problems.leetcode.easy.reverse_string import reverse_string
 scenarios("problems/leetcode/easy/Reverse String.feature")
 
 
-@given(parsers.parse("an array of {chars}"), target_fixture="context")
+@given(parsers.parse("an array of {chars}"), converters={"chars": lambda s: s.split(",")}, target_fixture="context")
 def given_an_array_of_chars(chars):
     return {"chars": chars}
 
@@ -16,7 +16,7 @@ def when_I_call_reverseString(context):
     chars = context["chars"]
     copy = list(chars)
     context["result"] = reverse_string(copy)
-    context["reeversed"] = copy
+    context["reversed"] = copy
 
 
 @then("the void function returns None")
@@ -25,9 +25,12 @@ def then_it_returns_None(context):
     assert_that(result, is_(None))
 
 
-@then(parsers.parse("the same array is procedurally {expected_reversed} by reference"))
+@then(
+    parsers.parse("the same array is procedurally {expected_reversed} by reference"),
+    converters={"expected_reversed": lambda s: s.split(",")},
+)
 def then_the_string_got_reversed(expected_reversed, context):
     reversed = context["reversed"]
-    reversed_chars = reversed.split(",")
+    reversed_chars = reversed
 
     assert_that(expected_reversed, equal_to(reversed_chars))
