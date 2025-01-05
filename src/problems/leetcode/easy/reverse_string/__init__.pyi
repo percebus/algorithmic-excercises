865f2412ae6429909ad9c256c0aca9c2aad4af7a
JC Guerrero <JCystems@gMail.com>
2025-01-05 08:10:59 -0600

    fix(leetcode/easy/reverse_string): w/o mem alloc (#30)

---
+++ src/problems/leetcode/easy/reverse_string/__init__.pyi
@@ -0,0 +1,12 @@
+def reverse_string(s: list[str]) -> None:
+    """
+    reverse_string
+
+    Given an array of characters s, it modifies the same array.
+
+    Parameters:
+        - `s` (`list[str]`): An array of string characters.
+
+    Returns: `None`
+    """
+    ...
--- src/problems/leetcode/easy/reverse_string/v2.py
+++ src/problems/leetcode/easy/reverse_string/v2.py
@@ -1,10 +1,12 @@
+from math import floor
+
 from src.problems.leetcode.easy.reverse_string.protocol import SolutionProtocol
 
 
 class Solution:  # (SolutionProtocol)
     def reverseString(self, s: list[str]) -> None:
         """
-        v1.Solution.reverseString
+        v2.Solution.reverseString
 
         Given an array of characters s, it modifies the same array.
 
@@ -15,10 +17,14 @@ class Solution:  # (SolutionProtocol)
         """
 
         chars = s
-        # FIXME simplify do 1 trip, instead of 2
-        inverted = chars[::-1]
-        for idx, item in enumerate(inverted):
-            s[idx] = item  # type: ignore
+        size: int = len(chars)
+        top: int = size - 1
+        mid: int = floor(size / 2)
+        for idx in range(mid):
+            char_a: str = chars[idx]
+            char_b: str = chars[top - idx]
+            chars[top - idx] = char_a
+            chars[idx] = char_b
 
 
 solution: SolutionProtocol = Solution()
