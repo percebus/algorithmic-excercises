2f963d1ff178758bfdd868de93270cadb9d0d582
JC Guerrero <JCystems@gMail.com>
2025-01-05 11:57:32 -0600

    Revert "refactor(pipelines/build): as lock"
    This reverts commit 37df4470e2ed9fc52c50e564d82487aea1497f3a.

--- scripts/setup.ba.sh
+++ scripts/setup.ba.sh
@@ -16,7 +16,9 @@ bash ${scripts_path}/pipx/setup.ba.sh
 # poetry has its own plugins
 bash ${scripts_path}/poetry/plugin/add.ba.sh
 
-poetry install
+# Install dependencies from requirements{.release}.txt
+bash ${scripts_path}/pip/install.ba.sh ${target_config}
+bash ${scripts_path}/pip/freeze.ba.sh
 
 set +v
 set +e
