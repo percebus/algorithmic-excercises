bf6eb484e5eb66eb452d18e798cb7f0801db76ef
JC Guerrero <JCystems@gMail.com>
2025-01-07 08:49:13 -0600

    Update inject.ba.sh

--- scripts/pipx/inject.ba.sh
+++ scripts/pipx/inject.ba.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 set -e
+
+filename="requirements.pipx_inject.txt"
+if [[ -z $(grep '[^[:space:]]' $filename) ]]; then
+  echo "${filename} is empty, skipping..."
+  exit 0
+fi
+
 set -v
 
-pipx inject -r requirements.pipx_inject.txt
+# pipx inject --requirement requirements.pipx_inject.txt # FIXME
+cat ${filename} | sed 's/.*/"&"/' | xargs -n 1 pipx inject
 
 set +v
 set +e
