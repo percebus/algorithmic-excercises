cd0ef9151a05d5635eed47d20f5520b1747ed341
JCGuerrero <JCystems@gMail.com>
2025-01-18 13:21:43 -0600

    ci

--- scripts/prepare.ba.sh
+++ scripts/prepare.ba.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+enironment=$1
+echo "enironment:'${enironment}'"
+
 scripts_path="$(dirname "$(readlink -f "$0")")"
 echo "Script directory: ${scripts_path}"
 
@@ -9,6 +12,10 @@ set -x
 
 # Upgrades top-level dependencies, like pipx
 bash ${scripts_path}/gil/setup.ba.sh
+
+if [[ "${enironment}" == "ci" ]]; then
+    mv ${scripts_path}/setup/ci.ba.sh setup.ba.sh
+fi
 
 bash ./repos/commons/scripts/prepare.ba.sh
 
