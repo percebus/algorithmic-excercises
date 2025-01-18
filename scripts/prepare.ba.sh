6c51dce42278cf15acef53ddf409a4aa83b7412d
JCGuerrero <JCystems@gMail.com>
2025-01-18 14:34:57 -0600

    fw

--- scripts/prepare.ba.sh
+++ scripts/prepare.ba.sh
@@ -8,11 +8,14 @@ echo "environment: ${environment}"
 scripts_path="$(dirname "$(readlink -f "$0")")"
 echo "Script directory: ${scripts_path}"
 
-set -x
 
 if [ "${environment}" == "pre-docker" ]; then
+    set -x
     rm ${scripts_path}/setup.ba.sh
+    set +x
 fi
+
+set -x
 
 # Upgrades top-level dependencies, like pipx
 bash ${scripts_path}/pip/upgrade.ba.sh
