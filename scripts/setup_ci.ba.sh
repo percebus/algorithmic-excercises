965f1250d297d238ca9a5a65260d8bde35eba548
JCGuerrero <JCystems@gMail.com>
2025-02-16 16:38:43 -0600

    Update setup_ci.ba.sh

--- scripts/setup_ci.ba.sh
+++ scripts/setup_ci.ba.sh
@@ -8,7 +8,6 @@ echo "Script directory: ${scripts_path}"
 
 set -x
 
-# Upgrades top-level dependencies, like pipx
 bash ${scripts_path}/prepare.ba.sh
 bash ./_scripts/setup.ba.sh
 bash ./_scripts/poetry/install.ba.sh ci
