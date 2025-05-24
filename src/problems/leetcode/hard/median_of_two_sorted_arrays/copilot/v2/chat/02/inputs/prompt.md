14e74096abc0e1d13019aed20e911164417102b5
JCGuerrero <JCystems@gMail.com>
2025-05-24 04:27:07 -0500

    d

---
+++ src/problems/leetcode/hard/median_of_two_sorted_arrays/copilot/v2/chat/02/inputs/prompt.md
@@ -0,0 +1,2 @@
+Compare the solution you provided with this solutions.
+Which one is better?, why? give all your insights
---
+++ src/problems/leetcode/hard/median_of_two_sorted_arrays/copilot/v2/chat/02/inputs/v1.py
@@ -0,0 +1,31 @@
+import math
+
+from src.problems.leetcode.hard.median_of_two_sorted_arrays.protocol import SolutionProtocol
+
+"""
+SRC: https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+Given two sorted arrays nums1 and nums2
+of size m and n respectively,
+return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)). # TODO TEST
+"""
+
+
+class Solution(SolutionProtocol):
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        nums3 = nums1 + nums2
+        nums3.sort()
+        size = len(nums3)
+        mid = math.ceil(size / 2)
+        is_pair = (size % 2) == 0
+        idx = mid - 1
+
+        if not is_pair:
+            return nums3[idx]
+
+        # implicit else
+        return (nums3[idx] + nums3[idx + 1]) / 2
+
+
+solution = Solution()
