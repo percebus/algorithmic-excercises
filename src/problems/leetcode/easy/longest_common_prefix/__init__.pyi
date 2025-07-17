a65e6a3ac7cc4834397c5f29a0dd503b0c6fd14e
JCGuerrero <JCystems@gMail.com>
2025-07-16 21:43:14 -0500

    f

---
+++ src/problems/leetcode/easy/longest_common_prefix/__init__.pyi
@@ -0,0 +1,17 @@
+
+def longest_common_prefix(words: list[str]) -> str:
+    """longest_common_prefix.
+
+    Calculate the Longest Common Prefix.
+
+    Parameters
+    ----------
+        - `words:list[str]`.- Different words that might share a common prefix.
+
+    Returns
+    -------
+        - :result: str:
+            - The longest common prefix string amongst an array of strings.
+            - If there is no common prefix, it returns an empty string.
+    """
+    ...
---
+++ src/problems/leetcode/easy/longest_common_prefix/longest_common_prefix.rst
@@ -0,0 +1,11 @@
+Longest Common Prefix
+===================
+
+.. doctest::
+    >>> from src.problems.leetcode.easy.longest_common_prefix import longest_common_prefix
+
+    >>> longest_common_prefix(["flower", "flow", "flight"])
+    'fl'
+
+    >>> longest_common_prefix(["dog", "racecar", "car"])
+    ''
