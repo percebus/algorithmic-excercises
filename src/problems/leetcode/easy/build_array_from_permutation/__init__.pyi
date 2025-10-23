782c3810657ff2e4f5e8bb9eca5c40e7cc930936
JC Guerrero <JCystems@gMail.com>
2025-10-23 14:52:33 -0500

    feat(leet/easy): build-array-from-permutation (#79)
    # Change
    
    ## Issues
    
    - #76
    
    ## Summary
    
    Build Array from Permutation

---
+++ src/problems/leetcode/easy/build_array_from_permutation/__init__.pyi
@@ -0,0 +1,36 @@
+def build_array_from_permutation(nums: list[int]) -> list[int]:
+    """build_array_from_permutation.
+
+    Build an array from a permutation.
+
+    Parameters
+    ----------
+        - `nums:list[int]`.- The input `list` representing a permutation.
+
+    Returns
+    -------
+        - :result: list[int]:
+            - The resulting `list` built from the permutation.
+
+    Examples
+    --------
+        - Example 1:
+            - Input: `nums = [0,2,1,5,3,4]`
+            - Output: `[0,1,2,4,5,3]`
+            - Explanation: The array ans is built as follows:
+            ```python
+                ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+                    = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+                    = [0,1,2,4,5,3]
+            ```
+
+        - Example 2:
+            - Input: `nums = [5,0,1,2,3,4]`
+            - Output: `[4,5,0,1,2,3]`
+            - Explanation:  The array ans is built as follows:
+            ```python
+                ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+                    = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+                    = [4,5,0,1,2,3]
+            ```
+    """
---
+++ src/problems/leetcode/easy/build_array_from_permutation/v1.py
@@ -0,0 +1,13 @@
+from src.problems.leetcode.easy.build_array_from_permutation.protocol import SolutionProtocol
+
+
+class Solution(SolutionProtocol):
+    def buildArray(self, nums: list[int]) -> list[int]:
+        # fmt: off
+        return [
+            nums[nums[i]]
+            for i in range(len(nums))]
+        # fmt: on
+
+
+solution = Solution()
