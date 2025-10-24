690615a063b4a15363cd17c5d50d1841832da5ae
JCGuerrero <JCystems@gMail.com>
2025-10-24 15:17:28 -0500

    f

--- src/problems/leetcode/medium/rotate_image/v2.py
+++ src/problems/leetcode/medium/rotate_image/v2.py
@@ -5,7 +5,7 @@ from src.problems.leetcode.medium.rotate_image.typing import MatrixType
 # Google's Solution (Gemini?)
 class Solution(SolutionProtocol):
     def rotate(self, matrix: MatrixType) -> None:
-        rows = reversed(matrix)
+        rows = matrix
         num_of_columns: int = len(matrix)
         for i, _ in enumerate(rows):
             for j in range(i + 1, num_of_columns):  # Iterate through the upper triangle
